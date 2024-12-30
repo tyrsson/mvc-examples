e8e01382c47481467cbb06ecd5eaa5e8604ebf36
Joey Smith <jsmith@webinertia.net>
2024-12-30 06:04:25 -0600

    Login is working via Authentication Validator Refactored SignInForm to use auth_config for identity for element name as well as validator keys/values implemented factory for SignInForm Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- module/User/src/Controller/Container/LoginControllerFactory.php
+++ module/User/src/Controller/Container/LoginControllerFactory.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace User\Controller\Container;
 
+use Laminas\Form\FormElementManager;
 use Psr\Container\ContainerInterface;
 use User\Controller\LoginController;
+use User\Form\SignInForm;
 
 final class LoginControllerFactory
 {
     public function __invoke(ContainerInterface $container): LoginController
     {
-        return new LoginController();
+        return new LoginController(
+            $container->get(FormElementManager::class)->get(SignInForm::class)
+        );
     }
 }
